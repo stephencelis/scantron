54d118969c3a55a608d797821570ee4779e7237a
Stephen Celis <stephen@stephencelis.com>
2011-02-14 17:46:01 -0600

    Store range delimiter data.

--- lib/scantron/scanners/range_scanner.rb
+++ lib/scantron/scanners/range_scanner.rb
@@ -3,15 +3,17 @@ require 'number_scanner'
 class RangeScanner < Scantron::Scanner
   values = NumberScanner.rules.values_at :human, :rational, :integer, :float
   valued = /#{values.map { |r| r.regexp }.join '|'} ?/
-  regexp = /#{NumberScanner.rules[:human].regexp} ?(and|or|to) ?#{valued}/
+  regexp = /#{NumberScanner.rules[:human].regexp}( ?(?:-|and|or|to) ?)#{valued}/
   rule :range_with_human, regexp do |r|
+    r.data[:delimiter] = r.scanner[1]
     n = NumberScanner.scan r.to_s.sub(/-/, ' ')
     n.size == 2 && n.first < n.last ? Range.new(*n) : false
   end
 
   values.delete_at 0
   valued = /(#{values.map { |r| r.regexp }.join '|'} ?)/
-  rule :range_without_human, /#{valued} ?(-|and|or|to) ?#{valued}/ do |r|
+  rule :range_without_human, /#{valued}( ?(?:-|and|or|to) ?)#{valued}/ do |r|
+    r.data[:delimiter] = r.scanner[5]
     n = NumberScanner.scan r.to_s.sub(/-/, ' ')
     n.size == 2 && n.first < n.last ? Range.new(*n) : false
   end
